bf3c3fc73fd45fd2daea697b6267152251c389c5
joonaskiuru <joonas.kiuru@gmail.com>
2024-01-15 13:28:38 +0200

    fix: dir path

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use("/api/points", pointsRouter);
 app.use("/api/auth/", authenticationRouter);
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 let server = undefined;
